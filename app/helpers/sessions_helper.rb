372b6325d78cd614c8f6bac20ba403470941565d
miyukitamura <miyuki3672@icloud.com>
2022-03-26 15:46:17 +0000

    B8途中

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,7 +24,7 @@ module SessionsHelper
   
   def current_user
     if (user_id = session[:user_id])
-        @current_user ||= User.find_by(id:session[user_id]) # x=x+1  → x+=1
+        @current_user ||= User.find_by(id:user_id) # x=x+1  → x+=1
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
