9520b0a5efff2858fa2392c6c2e133e7f862157a
miyukitamura <miyuki3672@icloud.com>
2022-03-31 14:45:34 +0000

    3/31 タスク新規作成途中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,21 @@
 class TasksController < ApplicationController
   
   def new
-    
+    @task = Task.new
+  end
+  
+  def create 
+    params[:content]
+    redirect_to tasks_index_url
   end
   
   def index
-    @tasks = @user.tasks
+    @task = @current_user.tasks
   end
   
+  def show
+    @task = Task.find(params[:id])
+  end
   
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
+  
+  def edit 
+  end
+  
   def new
     @user = User.new
   end
