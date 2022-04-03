00f80e73bf0f8f1cec252dcf7af45c3f93a3f555
miyukitamura <miyuki3672@icloud.com>
2022-04-03 14:24:18 +0000

    4/3 (2)

--- db/migrate/20220403130626_create_tasks.rb
+++ db/migrate/20220403130626_create_tasks.rb
@@ -1,10 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
-      t.string :name
-      t.datetime :created_at
-      t.string :description
-      t.references :user, foreign_key: true
+      t.text :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220327131606) do
+ActiveRecord::Schema.define(version: 20220403130626) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.text "content"
     t.datetime "created_at", null: false
-    t.string "description"
-    t.integer "user_id"
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
